9845e89cf77690987873e2873ba01c8642997d43
chalstead <chalstead@vmware.com>
2019-07-15 14:58:33 -0400

    Update Scripts

--- Horizon/Horizon.ps1
+++ Horizon/Horizon.ps1
@@ -16,32 +16,6 @@
   **This script and the App Volumes API is not supported by VMware**
   New sizes won't be reflected until a user logs in and attaches the Writable Volume	
   
-.EXAMPLE
- .\Expand-WV.ps1 
-        -AppVolumesServerFQDN "avmanager.company.com"
-        -AppVolumesDomain "mydomain" 
-        -AppVolumesUser "Username" 
-        -AppVolumesPassword "SecurePassword" 
-        -New_Size_In_MB "40960" 
-        -Update_WV_Size "yes" 
-
-    .PARAMETER AppVolumesServerFQDN
-    The FQDN of the App Volumes Manager where you want to view / change the Writable Volumes
-
-    .PARAMETER AppVolumesDomain
-    Active Directory Domain of the user with Administrative access
-
-    .PARAMETER AppVolumesUser
-    Active Directoty User with administrative access
-
-    .PARAMETER AppVolumesPassword
-    The password that is used by the user specified in the username parameter
-
-    .PARAMETER New_Size_In_MB
-    New size for the writable volumes in Megabytes. Take gigabytes and mutltiply by 1024.
-
-    .PARAMETER Update_WV_Size
-    Enter yes to update the sizes.  Type anything else for a list of writable volumes.
 #>
 
 
@@ -85,7 +59,6 @@ $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)
 $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
 $Credentials = '{"name":' + $username + ',"passwd":' + $UnsecurePassword +',"domain":' + $domain +'}' 
-$Script:Hash = @{}
 
 try {
     
